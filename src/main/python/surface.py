62388f7527fd0d762d37f47b2135feba160359b7
tjdragon <thierry@janaudy.com>
2019-04-27 20:20:08 +0800

    V1.0

--- src/main/python/surface.py
+++ src/main/python/surface.py
@@ -4,18 +4,23 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+print('Loading 1000x1000 grid')
+data = np.loadtxt('/home/tj/Downloads/skiing1000x1000.txt', delimiter=' ')
+#data = np.delete()
 
 def f(x, y):
-    return np.sin(np.sqrt(x ** 2 + y ** 2))
+    return data
 
-x = np.linspace(-6, 6, 30)
-y = np.linspace(-6, 6, 30)
+# numpy.linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None, axis=0)
+print('Creating linear spaces for x and y')
+x = np.linspace(start = 0, stop = 999, num = 1000, dtype = np.int)
+y = np.linspace(start = 0, stop = 999, num = 1000, dtype = np.int)
 
+print('Creating mesh grid')
 X, Y = np.meshgrid(x, y)
 Z = f(X, Y)
 
+print('Plotting the 3d surface')
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.contour3D(X, Y, Z, 50, cmap='binary')
